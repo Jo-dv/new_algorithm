7929931456c8b1476e39d1a0cea77427f37d3bc9
WOOJAE JO <63555689+Jo-dv@users.noreply.github.com>
2025-03-04 18:21:58 +0900

    [Silver III] Title: 게으른 백곰, Time: 256 ms, Memory: 71352 KB -BaekjoonHub

--- 백준/Silver/10025. 게으른 백곰/게으른 백곰.py
+++ 백준/Silver/10025. 게으른 백곰/게으른 백곰.py
@@ -1,25 +1,30 @@
+import sys
+
 class Main:
     def __init__(self):
-        self.n, self.k = map(int, input().split())
-        self.bottles = [list(map(int, input().split())) for _ in range(self.n)]
+        self.n, self.k = map(int, sys.stdin.readline().split())
+        self.bottles = [list(map(int, sys.stdin.readline().split())) for _ in range(self.n)]
         self.answer = 0
 
     def solve(self):
-        cage = [0] * (max(self.bottles, key=lambda cord: cord[1])[1] + 1)
+        MAX_X = 1000000  # 최대 좌표값
+        cage = [0] * (MAX_X + 1)  # 1차원 배열 생성 (초기화)
+
+        # 얼음 정보를 해당 위치에 저장
         for g, x in self.bottles:
-            cage[x] = g
+            cage[x] += g  # 같은 위치에 여러 개 있을 수 있으므로 +=
 
-        ice = 0
-        low = high = 0
-        while high < len(cage):
-            if high - low + 1 <= self.k * 2 + 1:
-                ice += cage[high]
-                high += 1
-            else:
-                ice -= cage[low]
-                low += 1
+        # 초기 윈도우 설정 (0 ~ min(K, MAX_X))
+        window_size = self.k * 2 + 1  # 왼쪽 K + 현재 위치 + 오른쪽 K
+        ice = sum(cage[:min(window_size, MAX_X + 1)])  # 초기 윈도우 합
 
-            self.answer = max(self.answer, ice)
+        self.answer = ice  # 초기 값 저장
+
+        # 슬라이딩 윈도우 진행
+        for i in range(window_size, MAX_X + 1):
+            ice += cage[i]  # 오른쪽 값 추가
+            ice -= cage[i - window_size]  # 왼쪽 값 제거
+            self.answer = max(self.answer, ice)  # 최대값 갱신
 
         print(self.answer)
 
