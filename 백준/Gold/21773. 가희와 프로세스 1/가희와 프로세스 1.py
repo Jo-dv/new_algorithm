e7d15ac869939ca0686aa688861bf35ad6854c18
WOOJAE JO <63555689+Jo-dv@users.noreply.github.com>
2025-03-06 20:31:23 +0900

    [Gold V] Title: 가희와 프로세스 1, Time: 2804 ms, Memory: 68848 KB -BaekjoonHub

--- 백준/Gold/21773. 가희와 프로세스 1/가희와 프로세스 1.py
+++ 백준/Gold/21773. 가희와 프로세스 1/가희와 프로세스 1.py
@@ -11,22 +11,24 @@ class Main:
     def solve(self):
         hq = []
         table = {}
+        current_time = 0
 
         for process in self.process:
             pc_id, run_time, priority = process
             heapq.heappush(hq, (-priority, pc_id))
             table[pc_id] = run_time
 
-        for _ in range(self.t):
+        while current_time < self.t:
             if hq:
                 priority, pc_id = heapq.heappop(hq)
                 print(pc_id)
-                if table[pc_id] - 1 > 0:  # 현재 프로세스를 실행했을 때 종료되지 않을 프로세스
+                if table[pc_id] - 1 > 0:
                     table[pc_id] -= 1
                     heapq.heappush(hq, (priority + 1, pc_id))  # 다른 프로세스 레벨을 높이지 말고 현재 프로세스 레벨을 낮춤
             else:
                 break
 
+            current_time += 1
 
 problem = Main()
 problem.solve()
