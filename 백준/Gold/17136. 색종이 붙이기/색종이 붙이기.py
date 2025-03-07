2a7031e34768ebf2d05df019aedbbb0c78a5f8e1
WOOJAE JO <63555689+Jo-dv@users.noreply.github.com>
2025-03-07 15:42:50 +0900

    [Gold II] Title: 색종이 붙이기, Time: 780 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.py
+++ 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.py
@@ -2,14 +2,13 @@ class Main:
     def __init__(self):
         self.grid = [list(map(int, input().split())) for _ in range(10)]
         self.paper = [5] * 5
-        self.answer = 26
+        self.answer = 26  # 최악의 경우 25개까지 사용 가능
 
-    def is_valid(self, sy, sx, ey, ex):  # 주어진 영역 안에 하나의 색종이를 붙일 수 있는지 확인
+    def is_valid(self, sy, sx, ey, ex):  # 해당 영역에 색종이를 붙일 수 있는지 확인
         for y in range(sy, ey + 1):
             for x in range(sx, ex + 1):
                 if not self.grid[y][x]:
                     return False
-
         return True
 
     def do_action(self, sy, sx, ey, ex, state):
@@ -17,24 +16,32 @@ class Main:
             for x in range(sx, ex + 1):
                 self.grid[y][x] = state
 
-    def search(self, cnt=0):
-        if self.answer <= cnt:  # 가지치기
+    def next_position(self, y, x):  # (y, x) 이후 첫 번째 1의 위치 찾기
+        for i in range(y, 10):
+            for j in range(10):
+                if i == y and j < x:  # 현재 줄에서는 x 이후부터 탐색
+                    continue
+                if self.grid[i][j]:
+                    return i, j
+        return -1, -1  # 더 이상 남은 1이 없으면 종료 조건
+
+    def search(self, y=0, x=0, count=0):
+        if count >= self.answer:  # 가지치기
             return
-        for y in range(10):
-            for x in range(10):
-                if self.grid[y][x]:
-                    for size in range(5):
-                        my, mx = y + size, x + size
-                        if self.paper[size] and my < 10 and mx < 10 and self.is_valid(y, x, my, mx):
-                            self.do_action(y, x, my, mx, 0)
-                            self.paper[size] -= 1
-                            self.search(cnt + 1)
-                            self.paper[size] += 1
-                            self.do_action(y, x, my, mx, 1)
 
-                    return  # 모든 색종이에 대해 확인을 끝냈다면 종료
+        ny, nx = self.next_position(y, x)
+        if ny == -1:  # 더 이상 덮을 1이 없으면 최적해 갱신
+            self.answer = min(self.answer, count)
+            return
 
-        self.answer = min(self.answer, cnt)
+        for size in range(5):
+            my, mx = ny + size, nx + size
+            if self.paper[size] and my < 10 and mx < 10 and self.is_valid(ny, nx, my, mx):
+                self.do_action(ny, nx, my, mx, 0)
+                self.paper[size] -= 1
+                self.search(ny, nx, count + 1)
+                self.paper[size] += 1
+                self.do_action(ny, nx, my, mx, 1)
 
     def solve(self):
         self.search()
