5b1a92c87ce322d870e35ce7e15bd421ff590c66
WOOJAE JO <63555689+Jo-dv@users.noreply.github.com>
2025-03-22 13:36:02 +0900

    [Silver II] Title: 랭킹전 대기열, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/20006. 랭킹전 대기열/랭킹전 대기열.py
+++ 백준/Silver/20006. 랭킹전 대기열/랭킹전 대기열.py
@@ -1,35 +1,33 @@
-from collections import defaultdict
-
-
 class Main:
     def __init__(self):
         self.p, self.m = map(int, input().split())
         self.players = [tuple(input().split()) for _ in range(self.p)]  # l, n = 레벨, 닉네임
+        self.game_room = dict()
+        self.room_info = dict()
+        self.idx = 0
+
+    def create_room(self, player):
+        level, name = player
+        level = int(level)
+        self.game_room[self.idx] = [player]
+        self.room_info[self.idx] = (level - 10, level + 10)
+        self.idx += 1
 
     def solve(self):
-        game_room = dict()
-        room_info = dict()
-        idx = 0
         for player in self.players:
-            level, name = player
-            level = int(level)
-            if not game_room:
-                game_room[idx] = [player]
-                room_info[idx] = (level - 10, level + 10)
-                idx += 1
+            if not self.game_room:
+                self.create_room(player)
             else:
-                for i in range(idx):
-                    if len(game_room[i]) == self.m:
+                for i in range(self.idx):
+                    if len(self.game_room[i]) == self.m:
                         continue
-                    if room_info[i][0] <= level <= room_info[i][1]:
-                        game_room[i].append(player)
+                    if self.room_info[i][0] <= int(player[0]) <= self.room_info[i][1]:
+                        self.game_room[i].append(player)
                         break
-                else:
-                    game_room[idx] = [player]
-                    room_info[idx] = (level - 10, level + 10)
-                    idx += 1
+                else:  # 방을 찾지 못한 플레이어
+                    self.create_room(player)
 
-        for room in game_room.values():
+        for room in self.game_room.values():
             room.sort(key=lambda x: x[1])
             print("Started!" if len(room) == self.m else "Waiting!")
             for player in room:
