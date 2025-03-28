97115546090edb623e041fa7da4243de94dcfaa3
WOOJAE JO <63555689+Jo-dv@users.noreply.github.com>
2025-03-28 10:20:45 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.21 ms, Memory: 9.38 MB -BaekjoonHub

---
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.py
@@ -0,0 +1,46 @@
+def search(place):
+    directions1 = [((-1, 0), (-2, 0)), ((1, 0), (2, 0)), ((0, -1), (0, -2)), ((0, 1), (0, 2))]
+    directions2 = [((0, -1), (-1, -1)), ((1, 0), (1, -1)), ((0, -1), (1, -1)), ((0, 1), (1, 1))]
+    people = []
+    # check = set()
+    
+    for i in range(5):
+        for j in range(5):
+            if place[i][j] == "P":
+                people.append((i, j))
+
+    for i in people:
+        y, x = i
+        for d in directions1:
+            flag = False
+            for dy, dx in d:
+                my, mx = y + dy, x + dx
+                if 0 <= my < 5 and 0 <= mx < 5:
+                    if place[my][mx] == "X":
+                        flag = True
+                    if not flag and place[my][mx] == "P" and abs(y - my) + abs(x - mx) <= 2:
+                        return 0
+        
+        for d in directions2:
+            flag = False
+            for dy, dx in d:
+                my, mx = y + dy, x + dx
+                if 0 <= my < 5 and 0 <= mx < 5:
+                    if place[my][mx] == "X":
+                        flag = True
+                    if not flag and place[my][mx] == "P" and abs(y - my) + abs(x - mx) <= 2:
+                        return 0
+                
+    return 1
+
+def solution(places):
+    answer = []
+    people = []
+    for i in range(5):
+        for j in range(5):
+            places[i][j] = list(places[i][j])
+            
+    for i in range(5):
+        answer.append(search(places[i]))
+
+    return answer
