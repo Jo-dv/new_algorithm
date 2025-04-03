727b81adad980796b46271d770f72548acc259e6
WOOJAE JO <63555689+Jo-dv@users.noreply.github.com>
2025-04-03 12:47:59 +0900

    [level 2] Title: 후보키, Time: 7.50 ms, Memory: 9.57 MB -BaekjoonHub

--- 프로그래머스/2/42890. 후보키/후보키.py
+++ 프로그래머스/2/42890. 후보키/후보키.py
@@ -9,7 +9,7 @@ def solution(relation):
         for comb in combinations(range(col), i):  # 유일성 체크
             temp = {tuple(item[j] for j in comb) for item in relation}
             if len(temp) == row:  # 중복이 없다면 유일성 만족
-                if not any(set(prev).issubset(set(comb)) for prev in candidates):  # 최소성 체크
+                if not any(set(candidate).issubset(set(comb)) for candidate in candidates):  # 최소성 체크 -> 후보키가 유일키의 서브셋이라면 해당 유일키는 후보키가 불가능
                     candidates.append(comb)
-    
+
     return len(candidates)
