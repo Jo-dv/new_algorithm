ca356d0922cc5921b50a6657c1e2b6aa8eb32b66
WOOJAE JO <63555689+Jo-dv@users.noreply.github.com>
2025-04-03 12:10:37 +0900

    [level 2] Title: 후보키, Time: 7.14 ms, Memory: 9.34 MB -BaekjoonHub

---
+++ 프로그래머스/2/42890. 후보키/후보키.py
@@ -0,0 +1,15 @@
+from itertools import combinations
+
+def solution(relation):
+    row = len(relation)
+    col = len(relation[0])
+    candidates = []
+    
+    for i in range(1, col + 1):  # 가능한 모든 속성 조합
+        for comb in combinations(range(col), i):  # 유일성 체크
+            temp = {tuple(item[j] for j in comb) for item in relation}
+            if len(temp) == row:  # 중복이 없다면 유일성 만족
+                if not any(set(prev).issubset(set(comb)) for prev in candidates):  # 최소성 체크
+                    candidates.append(comb)
+    
+    return len(candidates)
