3cb81522f77e16d415f07888a250d2ffcd03ef69
WOOJAE JO <63555689+Jo-dv@users.noreply.github.com>
2025-04-05 23:14:40 +0900

    [Silver II] Title: 창고 다각형, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.py
@@ -0,0 +1,34 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+
+class Main:
+    def __init__(self):
+        self.n = int(input())
+        self.info = [tuple(map(int, input().split())) for _ in range(self.n)]
+        self.answer = 0
+
+    def solve(self):
+        self.info.sort()
+        max_height = max(self.info, key=lambda h: h[1])
+        max_height_idx = self.info.index(max_height)
+
+        x, y = self.info[0]
+        for i in range(1, max_height_idx + 1):
+            if y < self.info[i][1]:
+                self.answer += ((self.info[i][0] - x) * y)
+                x, y = self.info[i]
+
+        self.answer += max_height[1]
+
+        x, y = self.info[-1]
+        for i in range(self.n-1, max_height_idx-1, -1):
+            if y <= self.info[i][1]:
+                self.answer += ((x - self.info[i][0]) * y)
+                x, y = self.info[i]
+
+        print(self.answer)
+
+
+problem = Main()
+problem.solve()
