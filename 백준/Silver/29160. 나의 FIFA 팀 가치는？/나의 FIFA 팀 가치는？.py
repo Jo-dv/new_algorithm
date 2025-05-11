16e8714c09408acae8d9e966540326fde3b7d4b9
WOOJAE JO <63555689+Jo-dv@users.noreply.github.com>
2025-05-11 22:02:55 +0900

    [Silver II] Title: 나의 FIFA 팀 가치는?, Time: 1928 ms, Memory: 242848 KB -BaekjoonHub

---
+++ 백준/Silver/29160. 나의 FIFA 팀 가치는？/나의 FIFA 팀 가치는？.py
@@ -0,0 +1,31 @@
+import sys
+import heapq
+input = lambda: sys.stdin.readline().rstrip()
+
+
+class Main:
+    def __init__(self):
+        self.n, self.k = map(int, input().split())
+        self.players = [list(map(int, input().split())) for _ in range(self.n)]
+        self.answer = 0
+
+    def solve(self):
+        squad = {i: [] for i in range(1, 12)}
+        for player in self.players:
+            p, w = player
+            heapq.heappush(squad[p], -w)
+
+        best = None
+        for _ in range(self.k):
+            best = [0] * 12
+            for i in range(1, 12):
+                if squad[i] and squad[i][0] < 0:
+                    player = heapq.heappop(squad[i])
+                    heapq.heappush(squad[i], player + 1)
+                    best[i] = -squad[i][0]
+
+        print(sum(best))
+
+
+problem = Main()
+problem.solve()
