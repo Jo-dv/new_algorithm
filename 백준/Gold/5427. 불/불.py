520568d6fabc29c18296b8bddd739a48e7150379
WOOJAE JO <63555689+Jo-dv@users.noreply.github.com>
2025-05-13 19:53:34 +0900

    [Gold IV] Title: 불, Time: 2328 ms, Memory: 86888 KB -BaekjoonHub

--- 백준/Gold/5427. 불/불.py
+++ 백준/Gold/5427. 불/불.py
@@ -21,24 +21,24 @@ class Main:
 
     def expand_fire(self):
         dq = deque([])
-        fire_info = [[0] * self.w for _ in range(self.h)]
+        fire_time = [[0] * self.w for _ in range(self.h)]
         for y in range(self.h):
             for x in range(self.w):
                 if self.grid[y][x] == "*":
                     dq.append((y, x))
-                    fire_info[y][x] = 1
+                    fire_time[y][x] = 1
 
         while dq:
             y, x = dq.popleft()
             for dy, dx in self.directions:
                 my, mx = y + dy, x + dx
-                if self.is_valid(my, mx) and self.grid[my][mx] == "." and fire_info[my][mx] == 0:
-                    fire_info[my][mx] = fire_info[y][x] + 1
+                if self.is_valid(my, mx) and self.grid[my][mx] == "." and fire_time[my][mx] == 0:
+                    fire_time[my][mx] = fire_time[y][x] + 1
                     dq.append((my, mx))
 
-        return fire_info
+        return fire_time
 
-    def search(self, init_y, init_x, fire_info):
+    def search(self, init_y, init_x, fire_time):
         dq = deque([(1, init_y, init_x)])
         visited = [[False] * self.w for _ in range(self.h)]
         visited[init_y][init_x] = True
@@ -46,25 +46,29 @@ class Main:
         while dq:
             time, y, x = dq.popleft()
             if y == 0 or y == self.h - 1 or x == 0 or x == self.w - 1:
-                print(time)
-                return
+                return time
 
             for dy, dx in self.directions:
                 my, mx = y + dy, x + dx
-                if self.is_valid(my, mx) and not visited[my][mx] and self.grid[my][mx] == "." and (time + 1 < fire_info[my][mx] or fire_info[my][mx] == 0):
-                    dq.append((time + 1, my, mx))
-                    visited[my][mx] = True
+                if self.is_valid(my, mx) and not visited[my][mx] and self.grid[my][mx] == ".":
+                    if time + 1 < fire_time[my][mx] or fire_time[my][mx] == 0:
+                        dq.append((time + 1, my, mx))
+                        visited[my][mx] = True
 
-        print("IMPOSSIBLE")
-        return
+        return "IMPOSSIBLE"
 
     def solve(self):
+        answers = []
+
         for _ in range(self.tc):
             self.w, self.h = map(int, input().split())
             self.grid = [list(input()) for _ in range(self.h)]
             y, x = self.find_start()
-            fire_info = self.expand_fire()
-            self.search(y, x, fire_info)
+            fire_time = self.expand_fire()
+            answer = self.search(y, x, fire_time)
+            answers.append(str(answer))
+
+        print("\n".join(answers))
 
 
 problem = Main()
